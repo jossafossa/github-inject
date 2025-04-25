348f541b3b9ff29dfba650f3d2475e8db2bb3d1f
Joost Hobma <joost.hobma@simplicate.nl>
2025-04-25 14:00:27 +0200

    Auto-commit on save

--- src/banner.js
+++ src/banner.js
@@ -1,10 +1,12 @@
+export const getBanner = () => `
 // ==UserScript==
 // @name         Github test
 // @namespace    http://tampermonkey.net/
-// @version      2025-04-25
+// @version      ${Date.now()}
 // @description  try to take over the world!
 // @author       You
 // @match        https://github.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
 // ==/UserScript==
+`;
