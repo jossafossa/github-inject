aa0bb89db444d278e124cb2e3b3f947657f58bc0
Joost Hobma <joost.hobma@simplicate.nl>
2025-04-25 13:49:35 +0200

    Auto-commit on save

---
+++ src/banner.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Github test
+// @namespace    http://tampermonkey.net/
+// @version      2025-04-25
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 // vite config
 import { defineConfig } from "vite";
+import banner2 from "rollup-plugin-banner2";
+// import { resolve } from "path";
 
 export default defineConfig({
+  plugins: [
+    banner2(
+      () => `
+    /**
+     * rollup-plugin-banner2
+     */
+    `
+    ),
+    // banner({
+    //   content: resolve(__dirname, "src/banner.js"),
+    // }),
+  ],
   // no hash in build
   build: {
     rollupOptions: {
