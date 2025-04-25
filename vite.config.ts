42c4a55155ee1ebc326a04c3d4c7dd9281f7c277
Joost Hobma <joost.hobma@simplicate.nl>
2025-04-25 13:56:38 +0200

    Auto-commit on save

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 // vite config
 import { defineConfig } from "vite";
 // import banner2 from "rollup-plugin-banner2";
-// import { resolve } from "path";
+import { resolve } from "path";
 import edit from "rollup-plugin-edit";
 
 export default defineConfig({
@@ -9,19 +9,7 @@ export default defineConfig({
     edit({
       chunk: (chunk) => {
         if (chunk.fileName === "index.js") {
-          const banner = `
-// ==UserScript==
-// @name         Github test
-// @namespace    http://tampermonkey.net/
-// @version      2025-04-25
-// @description  try to take over the world!
-// @author       You
-// @match        https://github.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
-// @grant        none
-// ==/UserScript==
-
-`;
+          const banner = resolve(__dirname, "src/banner.js");
           return banner + chunk.contents;
         }
       },
