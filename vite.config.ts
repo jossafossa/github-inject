3243db26e30b8b1a84b6b7c111c4293b49586710
Joost Hobma <joost.hobma@simplicate.nl>
2025-04-25 13:52:41 +0200

    Auto-commit on save

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,17 @@ export default defineConfig({
   plugins: [
     banner2(
       () => `
-    /**
-     * rollup-plugin-banner2
-     */
+// ==UserScript==
+// @name         Github test
+// @namespace    http://tampermonkey.net/
+// @version      2025-04-25
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
     `
     ),
   ],
