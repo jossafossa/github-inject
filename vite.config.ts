b90d66cfe458bab851a4dcc26f19bce759124d32
Joost Hobma <joost.hobma@simplicate.nl>
2025-04-25 13:57:52 +0200

    Auto-commit on save

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 // import banner2 from "rollup-plugin-banner2";
 import { resolve } from "path";
 import edit from "rollup-plugin-edit";
+import { readFileSync } from "fs";
 
 export default defineConfig({
   plugins: [
@@ -10,7 +11,11 @@ export default defineConfig({
       chunk: (chunk) => {
         if (chunk.fileName === "index.js") {
           // get the banner content from src/banner.js
-          const banner = resolve(__dirname, "src/banner.js");
+          const banner = readFileSync(
+            resolve(__dirname, "src/banner.js"),
+            "utf8"
+          );
+          // read the content of the file
           return banner + chunk.contents;
         }
       },
