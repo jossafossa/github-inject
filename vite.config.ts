6e46ba6f8a4d8782e7e42d505cb6fefab1eb7a4c
Joost Hobma <joost.hobma@simplicate.nl>
2025-04-25 13:56:05 +0200

    Auto-commit on save

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,18 @@ export default defineConfig({
     edit({
       chunk: (chunk) => {
         if (chunk.fileName === "index.js") {
-          const banner = `/*!
-          `;
+          const banner = `
+// ==UserScript==
+// @name         Github test
+// @namespace    http://tampermonkey.net/
+// @version      2025-04-25
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+`;
           return banner + chunk.output;
         }
       },
