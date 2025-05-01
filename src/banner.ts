a1496789e54a79e01ed5fbebbeef6b7c38b9e09d
Joost Hobma <joost.hobma@simplicate.nl>
2025-05-01 16:39:34 +0200

    Auto-commit on save

--- src/banner.ts
+++ src/banner.ts
@@ -11,5 +11,10 @@ export const getBanner = () => `// ==UserScript==
 // @updateURL    https://raw.githubusercontent.com/jossafossa/github-inject/refs/heads/master/dist/index.js
 // ==/UserScript==
 
-
+/*
+const GITHUB_CONFIG = {
+  username: "joosthobma", 
+  
+}
+*/
 `;
