319bf69a7f721a45c9d2bd2ba91f4a3f382bed09
Joost Hobma <joost.hobma@simplicate.nl>
2025-05-01 15:54:22 +0200

    Auto-commit on save

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Github test
 // @namespace    http://tampermonkey.net/
-// @version      1746107641620
+// @version      1746107662248
 // @description  try to take over the world!
 // @author       You
 // @match        https://github.com/*
