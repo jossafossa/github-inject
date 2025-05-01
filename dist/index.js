661e5869b5c9834834d77ce0ecdd22a8297ff885
Joost Hobma <joost.hobma@simplicate.nl>
2025-05-01 16:46:14 +0200

    Auto-commit on save

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Github Enhancer
 // @namespace    http://tampermonkey.net/
-// @version      1746110455729
+// @version      1746110774953
 // @description  Enhance your Github experience with additional features.
 // @author       Jossafossa
 // @match        https://github.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
+// @grant       GM_setValue
+// @grant       GM_getValue
 // @downloadURL  https://raw.githubusercontent.com/jossafossa/github-inject/refs/heads/master/dist/index.js
 // @updateURL    https://raw.githubusercontent.com/jossafossa/github-inject/refs/heads/master/dist/index.js
 // ==/UserScript==
--- src/banner.ts
+++ src/banner.ts
@@ -7,6 +7,8 @@ export const getBanner = () => `// ==UserScript==
 // @match        https://github.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
+// @grant       GM_setValue
+// @grant       GM_getValue
 // @downloadURL  https://raw.githubusercontent.com/jossafossa/github-inject/refs/heads/master/dist/index.js
 // @updateURL    https://raw.githubusercontent.com/jossafossa/github-inject/refs/heads/master/dist/index.js
 // ==/UserScript==
