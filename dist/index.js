646cbae4db2eb8c162b6e6db5a92f14ddfde7850
Joost Hobma <joost.hobma@simplicate.nl>
2025-05-01 14:00:34 +0200

    Auto-commit on save

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Github test
 // @namespace    http://tampermonkey.net/
-// @version      1746100804161
+// @version      1746100834065
 // @description  try to take over the world!
 // @author       You
 // @match        https://github.com/*
---
+++ src/mergeCheckbox.ts
@@ -0,0 +1,20 @@
+const LOCK_MERGE_STORAGE_KEY = "lock-merge";
+
+export const loadLockMergeCheckbox = () => {
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.id = "lock-merge";
+
+  document.body.appendChild(checkbox);
+
+  if (localStorage.getItem(LOCK_MERGE_STORAGE_KEY) === "checked") {
+    checkbox.checked = true;
+  }
+
+  checkbox.addEventListener("change", () => {
+    localStorage.setItem(
+      LOCK_MERGE_STORAGE_KEY,
+      checkbox.checked ? "checked" : "unchecked"
+    );
+  });
+};
