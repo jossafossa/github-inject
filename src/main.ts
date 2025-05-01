b1db289c2cdfb62e8f97423591c2cf6358efabc8
Joost Hobma <joost.hobma@simplicate.nl>
2025-05-01 14:00:04 +0200

    Auto-commit on save

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { loadStatus } from "./status";
 import styles from "./styles.scss?raw";
 import { waitFor } from "./utils";
 
@@ -72,31 +73,6 @@ const loadActionsButton = () => {
   message.innerText = "Actions of this PR";
 
   messages.append(message);
-};
-
-const getStatus = () => {
-  if (document.querySelector(".gh-header-meta [title='Status: Merged']"))
-    return "merged";
-  if (document.querySelector("[reviewable_state='draft']")) return "draft";
-  if (document.querySelector("[data-name*='QA-Test'], [data-name*='qa-test']"))
-    return "test";
-  return false;
-};
-
-const loadStatus = () => {
-  let updated = false;
-  const interval = setInterval(() => {
-    const status = getStatus();
-    if (updated !== status && status) {
-      const statusLabel = `[${status.toUpperCase()}]`;
-      updated = !!status;
-      const title = document.querySelector("h1 > bdi");
-      if (title) title.innerHTML = `${statusLabel} ${title.innerHTML}`;
-
-      document.title = `${statusLabel} ${document.title}`;
-      clearInterval(interval);
-    }
-  }, 1000);
 };
 
 const githubLoaded = async () => {
