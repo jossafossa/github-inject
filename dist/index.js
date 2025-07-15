aa3151e35c672726c2a7193786429702ba8202df
Joost Hobma <joost.hobma@simplicate.nl>
2025-07-15 13:52:46 +0200

    Auto-commit on save

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Github Enhancer
 // @namespace     http://tampermonkey.net/
-// @version       1752580357398
+// @version       1752580366433
 // @description   Enhance your Github experience with additional features.
 // @author        Jossafossa
 // @match         https://github.com/*
