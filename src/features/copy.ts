9c01cff23bed0664c54128242689ca1f59182bd2
Joost Hobma <joost.hobma@simplicate.nl>
2025-07-15 13:44:49 +0200

    Auto-commit on save

--- src/features/copy.ts
+++ src/features/copy.ts
@@ -18,7 +18,7 @@ const createCopyLink = (anchor: HTMLAnchorElement) => {
         copyLink.textContent = "Copied!";
         setTimeout(() => {
           copyLink.textContent = content;
-        }, 1000);
+        }, 500);
         console.log("Copied: " + anchor.textContent);
       })
       .catch((err) => {
